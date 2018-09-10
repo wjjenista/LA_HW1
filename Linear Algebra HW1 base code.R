84026cdbd48d50c3cf9ef1f71efb1210de853c5e
wjjenista <42449580+wjjenista@users.noreply.github.com>
2018-09-10 14:23:51 -0400

    fixes
    Got colors to plot, added ggplot library

--- Linear Algebra HW1 base code.R
+++ Linear Algebra HW1 base code.R
@@ -1,4 +1,5 @@
 library(RColorBrewer)
+library(ggplot2)
 
 setwd("C:\\Users\\Bill\\Documents\\NCSU\\Course Work\\Fall\\Linear Algebra\\Homework")
 
@@ -14,11 +15,8 @@ leuk.pca = prcomp(leuk[,1:5000], scale=TRUE)
 display.brewer.all()
 palette(brewer.pal(n = 3, name = "Set1"))
 
-## Plot first 2 PCA components   !!!!!!! I couldn't get different colors for the 3 different leukemia's; help please !!!!!!!
-plot(leuk.pca$x[,1], leuk.pca$x[,2], xlab = "Principal Component 1", ylab = "Principal Component 2")
-
-## Do a biplot --> this did not work out so well; too many vectors projected onto the 2-D space (5000 original vectors!)
-biplot(leuk.pca, choices = 1:2)
+## Plot first 2 PCA components
+plot(leuk.pca$x[,1], leuk.pca$x[,2], col=leuk$V5001, xlab = "Principal Component 1", ylab = "Principal Component 2")
 
 ## Calculate percent variation captured
 sum(leuk.pca$sdev[1:2]^2)/sum(leuk.pca$sdev^2)
